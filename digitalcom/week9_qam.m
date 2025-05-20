75e6d42135b00e762d52a1d524736605674fed45
현오홍 <harrymickey@naver.com>
2025-05-20 16:09:16 +0900

    코드 추가

--- digitalcom/week9_qam.m
+++ digitalcom/week9_qam.m
@@ -134,47 +134,4 @@ end
 figure
 plot(p_offset)
 
-%%
-
-
-%%
-% alphabet=[exp(1j*pi*(1/4)),exp(1j*pi*(3/4)),exp(1j*pi*(5/4)),exp(1j*pi*(7/4))];
-% 
-% qpsk=randsrc(4000,1,alphabet);
-% 
-% qpsk_up=upsample(qpsk,10);
-% 
-% T=1/10^5;
-% t=-10*T:T/10:10*T;
-% beta2=0.5;
-% 
-% Fs=10/T;
-% F=linspace(-Fs/2,Fs/2,201);
-% 
-% RC_filter2=sinc(t./T).*cos(pi*beta2*t./T)./(1-(2*beta2*t./T).^2);
-% 
-% qpsk_conv=conv(qpsk_up,RC_filter2,"same");
-% 
-% qpsk_sample=qpsk_conv(1:10:length(qpsk_conv));
-% 
-% qpsk_I=real(qpsk_sample);
-% qpsk_Q=imag(qpsk_sample);
-% 
-% figure
-% scatter(qpsk_I,qpsk_Q);
-% grid on
-% 
-% qpsk_eye=real(qpsk_conv);
-% 
-% figure
-% 
-% for i=1:49
-%     plot(qpsk_eye(20*(i-1)+1:20*i+1));
-%     hold on
-%     grid on
-% end
-% 
-% figure
-% plot(qpsk_conv);
-% grid on
 
